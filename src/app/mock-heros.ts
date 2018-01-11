24f912f4e78cce350f6af2ba2d0fcfe48e472ad3
Amanda Mcclintock <Amanda.Mcclintock@flex.com>
2018-01-11 15:49:21 -0500

    Adding heros

---
+++ src/app/mock-heros.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROS: Hero[] = [
+    { id: 11, name: 'Tracer' },
+    { id: 12, name: 'Mercy'  },
+    { id: 13, name: 'Hanzo'  },
+    { id: 14, name: 'Genji'  },
+    { id: 15, name: 'Reinhardt' },
+    { id: 16, name: 'D.Va' },
+    { id: 17, name: 'Sombra' },
+    { id: 18, name: 'Doomfist' },
+    { id: 19, name: 'Reaper' },
+    { id: 20, name: 'McCree' }
+
+];
